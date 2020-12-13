d2ab009d6dada26d56d35f38951d87641080b3e2
MatsuYuu <matsu_doit@icloud.com>
2020-12-13 17:12:45 +0900

    バリデーション追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   
   belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :genre_id
+    validates :artwork
+    validates :evaluation_id
+    validates :text
+     
 end
