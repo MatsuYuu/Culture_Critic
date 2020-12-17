d6733cbdf266074731fc0f14e5193707afc03a3c
MatsuYuu <matsu_doit@icloud.com>
2020-12-17 22:24:24 +0900

    投稿編集・削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,25 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to root_path
+  end
+
+
   private
 
   def comment_params
